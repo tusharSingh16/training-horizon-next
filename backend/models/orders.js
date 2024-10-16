50239a53e12c63ae76a83ba398f783320a30856f
Sakcham Nainwal <sakcham@Sakchams-MacBook-Air.local>
2024-10-16 21:38:52 +0530

    order details

---
+++ backend/models/orders.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const checkoutSchema = new mongoose.Schema(
+  {
+    // _id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+    },
+    phone: { type: String, required: true },
+    address: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+    paymentMethod: {type: String, required: true},
+    orderNotes: { type: String },
+    status: {
+        type: String,
+        default: "ON HOLD",
+        required: false,
+    },
+    courseTitle:{
+        type: String,
+        required: true,
+    },
+    coursePrice: {
+        type: String,
+        required: true
+    }
+    // title: {type: String}, 
+    // totalPrice: {type: String}
+  },
+
+  { timestamps: true }
+);
+
+const Orders = mongoose.model("orders", checkoutSchema);
+module.exports = {
+  Orders
+}
---
+++ backend/routes/orderRouter.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const { Orders } = require("../models/orders");
+const zod = require("zod");
+const {authMiddleware} = require("../middleware/authMiddleware")
+
+router.post("/checkout", authMiddleware, async function (req, res) {
+  const userId = req.userId
+  try {
+    const newOrderData = {
+      ...req.body,
+      user: userId,  // Add the userId to the order data
+    };
+    const newOrder = new Orders(newOrderData);
+    await newOrder.save();
+    res.status(201).json(newOrder);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.get("/getOrdersByUserId/:userId", async function (req, res){
+  const {userId} = req.params;
+  try{
+    const orders = await Orders.find({user: userId});
+    if(!orders){
+      return res.status(404).json({
+        message: "User Not Found",
+      })
+    }
+
+    res.status(200).json({
+      message: "Orders retrieved successfully",
+      orders,
+    })
+  }
+  catch(error){
+    res.status(500).json({
+      message: "Error fetching orders",
+      error: error,
+    })
+  }
+})
+
+router.get("/getOrderById/:orderId", async function (req, res){
+  const {orderId} = req.params;
+  try{
+    const orders = await Orders.findById(orderId);
+    if(!orders){
+      return res.status(404).json({
+        message: "Order Not Found",
+      })
+    }
+
+    res.status(200).json({
+      message: "Order retrieved successfully",
+      orders,
+    })
+  }
+  catch(error){
+    res.status(500).json({
+      message: "Error fetching order",
+      error: error,
+    })
+  }
+})
+
+
+module.exports = router;
